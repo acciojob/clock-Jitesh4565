c9528596a4c36823a6243409270b519ae1a60144
Jitesh4565 <73875204+Jitesh4565@users.noreply.github.com>
2023-11-24 17:19:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function time(){
+	const newTime=document.getElementById("timer");
+	let date=new Date();
+ newTime.innerText=date.toLocaleDateString()+","+date.toLocaleTimeString();
+	setTimeout(time,1000);
+}
+time();
